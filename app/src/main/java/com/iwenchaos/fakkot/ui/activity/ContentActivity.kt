ea43828a9f0c2b309375465445a59be56a62567b
iwenchao <iwenchaos@gmail.com>
2018-04-05 22:04:15 +0800

    二级页面的web处理

--- app/src/main/java/com/iwenchaos/fakkot/ui/activity/ContentActivity.kt
+++ app/src/main/java/com/iwenchaos/fakkot/ui/activity/ContentActivity.kt
@@ -1,7 +1,9 @@
 package com.iwenchaos.fakkot.ui.activity
 
+import android.os.Bundle
 import com.iwenchaos.fakkot.R
 import com.iwenchaos.fakkot.base.BaseActivity
+import com.just.agentweb.AgentWeb
 
 /**
  * Created by chaos
@@ -9,7 +11,32 @@ import com.iwenchaos.fakkot.base.BaseActivity
  * 文件描述：
  */
 class ContentActivity : BaseActivity() {
-    override fun setLayoutId(): Int = R.layout.activity_splash
+
+    private lateinit var agentWeb :AgentWeb
+    private lateinit var shareTitle:String
+    private lateinit var shareUrl :String
+    private var  shareId : Int = 0
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+
+    /**
+     * 初始化web页的immersion bar
+     */
+    override fun initImmersionBar() {
+        super.initImmersionBar()
+        immersionBar.titleBar(R.id.toolbar).init()
+    }
+
+
+    override fun setLayoutId(): Int = R.layout.activity_content
 
     override fun cancelRequest() {
     }
