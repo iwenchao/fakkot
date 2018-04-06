b468b00ba484c8cbf97ab0b4cc93374efd5bd489
iwenchao <iwenchaos@gmail.com>
2018-04-06 10:57:01 +0800

    分类页面的详情布局

--- app/src/main/java/com/iwenchaos/fakkot/ui/activity/TypeContentActivity.kt
+++ app/src/main/java/com/iwenchaos/fakkot/ui/activity/TypeContentActivity.kt
@@ -1,17 +1,24 @@
 package com.iwenchaos.fakkot.ui.activity
 
+import android.os.Bundle
 import com.iwenchaos.fakkot.R
 import com.iwenchaos.fakkot.base.BaseActivity
 
 /**
  * Created by chaos
  * on 2018/3/24. 18:04
- * 文件描述：
+ * 文件描述：分类资料
  */
 class TypeContentActivity : BaseActivity() {
 
 
-    override fun setLayoutId(): Int = R.layout.activity_splash
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+    override fun setLayoutId(): Int = R.layout.activity_type_content
 
     override fun cancelRequest() {
     }
