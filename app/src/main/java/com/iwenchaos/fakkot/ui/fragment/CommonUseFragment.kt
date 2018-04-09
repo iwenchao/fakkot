5561b426a0e42f7609f2b83fc62c09e580e994ae
iwenchao <iwenchaos@gmail.com>
2018-04-09 22:32:28 +0800

    增加kotterknife

--- app/src/main/java/com/iwenchaos/fakkot/ui/fragment/CommonUseFragment.kt
+++ app/src/main/java/com/iwenchaos/fakkot/ui/fragment/CommonUseFragment.kt
@@ -1,12 +1,16 @@
 package com.iwenchaos.fakkot.ui.fragment
 
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.iwenchaos.fakkot.R
 import com.iwenchaos.fakkot.base.BaseFragment
 import com.iwenchaos.fakkot.bean.FriendListResponse
+import com.iwenchaos.fakkot.inflate
+import com.zhy.view.flowlayout.TagFlowLayout
 
 /**
  * Created by chaos
@@ -16,13 +20,24 @@ import com.iwenchaos.fakkot.bean.FriendListResponse
 class CommonUseFragment : BaseFragment() {
 
     private var mainView: View? = null
+    private lateinit var flowLayout: ConstraintLayout
+
+    private lateinit var hotTagFlowLayout:TagFlowLayout
+    private lateinit var commonUseTagFlowLayout:TagFlowLayout
+    private lateinit var bookmarkTagFlowLayout:TagFlowLayout
+    private lateinit var hotTitle:TextView
+    private lateinit var commonUseTitle:TextView
+    private lateinit var bookmarkTitle:TextView
     private val datas = mutableListOf<FriendListResponse.Data>()
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         mainView?.let {
             mainView = inflater?.inflate(R.layout.fragment_common_use, container, false)
-
+            flowLayout = activity.inflate(R.layout.common_hot) as ConstraintLayout
+            hotTagFlowLayout = flowLayout.findViewById<TagFlowLayout>(R.id.hotFlowLayout)
+            commonUseTagFlowLayout = flowLayout.findViewById<TagFlowLayout>(R.id.commonUseFlowLayout)
+            bookmarkTagFlowLayout = flowLayout.findViewById<TagFlowLayout>(R.id.bookmarkFlowLayout)
         }
         return mainView
     }
